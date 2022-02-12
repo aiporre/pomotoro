2a89af3ad3937084d172de14b5971d1d1772f144
aiporre <ariel.iporre.rivas@gmail.com>
2022-02-13 00:39:38 +0100

    created base function to load task

--- db.js
+++ db.js
@@ -19,5 +19,33 @@ var closeConn = (db) => {
 	});
 }; 
 
-let db = getConn()
-closeConn(db)
+
+var createTask = (name, description, estimation ) => {
+	task = {"name": name, 
+		"description": description,
+		"efford": 0 }
+	let conn = getConn();
+	db.run('INSERT INTO tasks(name, description, efford) VALUES(?,?,?)',
+		[task.name, task.description, task.efford],
+		(err) => {
+			if(err){
+				return console.error(err.message);
+			};
+			console.log(`A new task was created" with id ${this.lastID}`);
+		}
+	);
+	closeConn(db);
+
+}
+let db = getConn();
+
+db.run('CREATE TABLE tasks(task_id INTEGER PRIMARY KEY, name TEXT, description TEXT, efford INTEGER)', (err) => {
+	if(err) {
+		return console.error('Error when table created')
+	};
+	console.log('created table of tasks');
+});
+
+createTask("prepare lunch", "turn oven and insert frozen pizza", 10);
+closeConn(db);
+
